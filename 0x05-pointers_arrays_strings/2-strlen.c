9301bdefcaa0b001e37049c9eb8bbc6cdb702578
priest-tech <shixkamanu@gmail.com>
2022-07-13 05:54:00 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - give the leght of a string.
+ *@s: character to check.
+ *
+ * Return: leghth of string.
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i=0; s[i] != '\0'; i++);
+
+	return (i);
+}
+
