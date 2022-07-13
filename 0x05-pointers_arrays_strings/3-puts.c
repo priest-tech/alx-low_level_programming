fba08bb7d5139a5d3ebcb115a38a07042c82e944
priest-tech <shixkamanu@gmail.com>
2022-07-13 06:53:00 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _puts - create a string followed by a new line.
+ *
+ * Return: string.
+ */
+void _puts(char *str)
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+		if (str[count] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		_putchar(str[count]);
+		count++;
+	}
+}
+
