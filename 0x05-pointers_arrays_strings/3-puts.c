ab75a3e69a03723b64d0d8476f3f9ad5fa613b22
priest-tech <shixkamanu@gmail.com>
2022-07-13 07:00:24 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * _puts - create a string followed by a new line.
- *
- * Return: string.
+ * _puts - prints a string, followed by a new line.
+ * @str: input string.
+ * Return: no return.
  */
 void _puts(char *str)
 {
