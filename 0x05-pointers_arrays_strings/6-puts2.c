edc464c5cba5ea123b35b3e3d90dc93418a23dd8
priest-tech <shixkamanu@gmail.com>
2022-07-13 09:11:17 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every character starting with the first, and new line.
+ * @str: input string.
+ *
+ * Return: 0.
+ */
+void puts2(char *str);
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+		if(str[count] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		if (count % 2 == 0)
+			_putchar(str[count]);
+		count++;
+	}
+}
