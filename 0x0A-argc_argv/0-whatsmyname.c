98c50d9b0fc517cd85c37301521507015f1b7964
priest-tech <shixkamanu@gmail.com>
2022-07-25 01:44:05 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,15 @@
-#include "main.h"
 #include <stdio.h>
-int main (int argc, char *argv[])
+
+/**
+ * Main - Prints the program name, followed by a new line.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: Always o.
+ */
+int main(int argc, char *argv[])
 {
-	printf( "argc, %s/n", argc)
-		return = 0;
+	printf("%s\n", argv[0]);
+
+	return (0);
 }
