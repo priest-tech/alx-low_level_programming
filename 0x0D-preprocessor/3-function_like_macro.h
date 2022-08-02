81131da6bce12b57ce2667a1efc31e8fc5e7fb33
priest-tech <shixkamanu@gmail.com>
2022-08-02 21:38:51 +0300

    3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,4 @@
-#ifndef FUNCTIOIN_LIKE_MACRO_H
-#define FUNCTION_LIKE_MACRO_H
-#define ABS(x) (((x)<0) ? - (x) : (x))
-#endif
+#ifndef THREE_FUNCTION_LIKE_MACRO_H
+#define THREE_FUNCTION_LIKE_MACRO_H
+#define ABS(x) (x > 0 ? (x) : (x) * -1)
+#endif /* THREE_FUNCTION_LIKE_MACRO_H */
