b5a2c5eef329ee7f39e1529737c56749295b417f
priest-tech <shixkamanu@gmail.com>
2022-08-12 00:36:20 -0700

    all

---
+++ 0x12-singly_linked_lists/tests/100-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - check the codes.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("(God is good all the time, all the time God is good.)\n");
+	return (0);
+}
